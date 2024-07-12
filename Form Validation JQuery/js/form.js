40fa50b5f1d8ffd85d023fc92fa66f969c9712fb
anandvishnu2001 <anandvishnu@techversantinfotech.com>
2024-07-12 11:11:40 +0530

    Updated Form Validation JQuery
    Updated on 12-07-2024

--- Form Validation JQuery/js/form.js
+++ Form Validation JQuery/js/form.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-	let valid = true;
 
-	let check = true;
+	let nameChecked = [ "Gender" , "Marital-Status" , "Terms-and-Conditions" ];
 
-	let idcheck = [ "email" , "password" , "phone" ]
+	let checkedCount = [ 0 , 0 , 0 ];
+
+	let idCheck = [ "email" , "password" , "phone" ];
 
 	let checker = [ /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/ ,
 			/^([A-Za-z0-9!@#$%^&*()_+-]){8,}$/ ,
@@ -13,30 +14,66 @@ $(document).ready(function(){
 			"*Password should have atleast 8 characters<br>alphabets, numbers and !@#$%^&*()_+-" ,
 			"*Phone should have only 10 numbers" ];
 
+	let check = [ true , true , true ];
 
 	$(".check").on("input",function(){
 		let id = $(this).attr("id");
-		for( i in idcheck ){
-			if(id === idcheck[i]){
-				if(!$("#"+id).val().match(checker[i]))
-					$("#"+id+"pop").html(pop[i]).css("color","red").show();
-				else
-					$("#"+id+"pop").html("*Valid form verified").css("color","green").show();
+		for(let i in idCheck){
+			if(id === idCheck[i]){
+				if(!$("#"+id).val().match(checker[i])){
+					check[i] = false;
+					$("#"+id+"pop").html(pop[i]).css("color","red");
+				}
+				else{
+					check[i] = true;
+					$("#"+id+"pop").html("*Valid form verified").css("color","green");
+				}
 			}
 		}
 	});
 
 	$("form").submit(function(e){
-		if($(".valid").val() == '' || $(".valid").val() == null){
-			valid = false;
-			$("#"+$(this).attr("id")+"pop").html("*"+$(this).attr("name")+" field is required").css("color","red");
+		let valid = true;
+
+		$(".valid").each(function(){
+			if($(this).val() == '' || $(this).val() == null){
+				valid = false;
+				$("#"+$(this).attr("id")+"pop").html("*"+$(this).attr("name")+" field is required").css("color","red");
+			}
+			else if($(this).hasClass("check")){
+				let i = idCheck.indexOf($(this).attr("id"));
+				if(!check[i]){
+					valid = false;
+					$("#"+$(this).attr("id")+"pop").html("*"+$(this).attr("name")+" field form is invalid").css("color","red");
+				}
+			}
+			else
+				$("#"+$(this).attr("id")+"pop").html("*Valid form is verified").css("color","green");
+
+		});
+
+		$(":radio,:checkbox").each(function(){
+			for(let i in nameChecked)
+				if($(this).attr("name") === nameChecked[i])
+					if($(this).prop('checked') == true)
+						checkedCount[i] += 1 ;
+		});
+		for(let i in checkedCount){
+			if(checkedCount[i] === 0){
+				valid = false;
+				$("#"+nameChecked[i]+"pop").html("*"+nameChecked[i]+" field is required").css("color","red");
+			}
+			else
+				$("#"+nameChecked[i]+"pop").html("*Valid form is verified").css("color","green");
 		}
-		else
-			$("#"+$(this).attr("id")+"pop").html("*Valid form is verified").css("color","green");
 
 		if(!valid){
-			$(".pop").show();
+			alert("All fields are mandatory");
 			e.preventDefault();
+			for(let i in checkedCount)
+				checkedCount[i] = 0 ;
 		}
+		else
+			alert("Form is valid");
 	});
 });
