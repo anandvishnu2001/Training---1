2c340a7f609c2163b9f95db9c76479be2a651342
anandvishnu2001 <anandvishnu@techversantinfotech.com>
2024-07-18 14:15:06 +0530

    Updated JQuery Tasks

--- JQuery Tasks/js/design.js
+++ JQuery Tasks/js/design.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
 
-	$("#popup").hide();
-
 	$(".tab").click(function(){
 		$(".tabcontent").hide();
 		$(".tab").not(this).removeClass("active");
 		$(this).addClass("active");
-		$("#"+$(this).attr("name")).fadeIn("slow");
+		$(`#${$(this).attr("name")}`).fadeIn("slow");
 	});
 
 	$(".accordion").click(function(){
 		$(this).toggleClass("active");
-		$("#"+$(this).attr("name")).toggle("slow");
+		$(`#${$(this).attr("name")}`).toggle("slow");
 	});
+
+	$("#datepicker").datepicker();
+
+	$("#datatable").DataTable();
 
 });
