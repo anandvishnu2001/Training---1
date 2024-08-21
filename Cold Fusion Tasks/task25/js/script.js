60b5caf3d45151a274a518b3dc99f3efdce9cd48
anandvishnu2001 <anandvishnu@techversantinfotech.com>
2024-08-21 17:34:41 +0530

    Updated

---
+++ Cold Fusion Tasks/task25/js/script.js
@@ -0,0 +1,13 @@
+ /*$(document).ready(function() {
+		let substring = $('.style').html().split('-')[1].split('(')[0];
+					alert(substring);
+		/*$.ajax({
+			url: './index.cfm',
+			type: 'POST',
+			dataType: 'json',
+			success: function(response) {
+					alert(response);
+				 }
+		});*/
+});*/	
+					alert('substring');
---
+++ Cold Fusion Tasks/task27/index.cfm
@@ -0,0 +1,3 @@
+<form action="" method="post">
+	<label>Enter Username:</label><input type="text">
+</form>
